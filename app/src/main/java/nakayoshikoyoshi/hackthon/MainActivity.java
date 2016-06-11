4cbe7797bf25924feb7873a65085a9eccea9e86e
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2016-06-11 17:25:00 +0900

    プルダウン出来たよ

--- app/src/main/java/nakayoshikoyoshi/hackthon/MainActivity.java
+++ app/src/main/java/nakayoshikoyoshi/hackthon/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Intent intent = new Intent(getApplication(), MapsActivity.class);
+        Intent intent = new Intent(getApplication(), avatar_setting.class);
         startActivity(intent);
 
         /*
